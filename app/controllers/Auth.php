417da91f0a3d3d9823ecfa66f63b60189f488def
joshanegenabe <genabejoshaneangelie@mail.com>
2025-09-29 07:50:57 +0800

    first commit

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -9,7 +9,7 @@ class Auth extends Controller {
         if ($this->io->post()) {
             $username = $this->io->post('username');
             $password = $this->io->post('password');
-            $user = $this->user_model->get_by_username($username);
+            $user = $this->User_model->get_by_username($username);
             if ($user && password_verify($password, $user['password'])) {
                 $_SESSION['user'] = $username;
                 redirect('author');
@@ -26,12 +26,12 @@ class Auth extends Controller {
         if ($this->io->post()) {
             $username = $this->io->post('username');
             $password = password_hash($this->io->post('password'), PASSWORD_DEFAULT);
-            $user = $this->user_model->get_by_username($username);
+            $user = $this->User_model->get_by_username($username);
             if ($user) {
                 $data['error'] = 'Username already exists.';
                 $this->call->view('register', $data);
             } else {
-                $this->user_model->create(['username' => $username, 'password' => $password]);
+                $this->User_model->create(['username' => $username, 'password' => $password]);
                 redirect('auth/login');
             }
         } else {
