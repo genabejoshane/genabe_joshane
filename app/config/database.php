084ca017faede7a47867e0ffce6fae35354b6132
joshanegenabe <genabejoshaneangelie@mail.com>
2025-09-29 10:22:14 +0800

    first commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799154',
-    'password'	=> 'TJPEC4wlYx',
-    'database'	=> 'sql12799154',
+    'username'	=> 'sql12799940',
+    'password'	=> 'sql12799940',
+    'database'	=> 'sql12799940',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
 );
